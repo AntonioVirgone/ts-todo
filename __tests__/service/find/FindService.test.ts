0c3680d4bc01da6a7b086241cdf22dc0ccc414db
antonio <antonio.virgone@telepass.com>
2024-07-20 19:03:09 +0200

    fix test

--- __tests__/service/find/FindService.test.ts
+++ __tests__/service/find/FindService.test.ts
@@ -1,27 +1,37 @@
 import { TodoElementModel } from "../../../src/model/TodoElement";
 import { TodoStatus } from "../../../src/model/TodoStatus";
+import { FindRepository } from "../../../src/repository/find/FindRepository";
 import { FindService } from "../../../src/service/find/FindService";
+
+// Creiamo un mock della classe FindRepository
+jest.mock("../../../src/repository/find", () => {
+  return {
+    FindRepository: jest.fn().mockImplementation(() => {
+      return {
+        findAll: jest.fn().mockResolvedValue([
+          {
+            _id: "abc",
+            title: "Title 1",
+            description: "lorem ipsum",
+            status: TodoStatus.COMPLETED,
+          },
+        ]),
+      };
+    }),
+  };
+});
 
 describe("FindService", () => {
   let service: FindService;
-
-  // Mock list
-  const mockList: TodoElementModel[] = [
-    {
-      _id: "abc",
-      title: "Title 1",
-      description: "lorem ipsum",
-      status: TodoStatus.COMPLETED,
-    },
-  ];
+  let repository: FindRepository;
 
   beforeEach(() => {
     service = new FindService();
   });
 
-  test("should find all element", () => {
-    const items = service.findAll();
+  test("should find all element", async () => {
+    const items = await service.findAll();
     expect(items).toHaveLength(1);
-    expect(items).toEqual(mockList);
+    expect(items).toEqual(repository.findAll());
   });
 });
