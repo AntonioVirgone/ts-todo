c0962c78cfd2ce0abd390e4fc3b528ba9e9bab5e
antonio <antonio.virgone@telepass.com>
2024-07-25 18:59:29 +0200

    add secrets token and authentication on all controllers

--- src/controller/delete/DeleteController.ts
+++ src/controller/delete/DeleteController.ts
@@ -1,13 +1,16 @@
+import { Auth } from "../../decorator/Auth";
 import { DeleteService } from "../../service/delete/DeleteService";
 import { IDeleteController } from "./IDeleteController";
 
 export class DeleteController implements IDeleteController {
   private deleteService: DeleteService = new DeleteService();
 
+  @Auth
   async delete(userCode: string): Promise<void> {
     return await this.deleteService.delete(userCode);
   }
 
+  @Auth
   async deleteById(userCode: string, itemId: string): Promise<void> {
     return await this.deleteService.deleteById(userCode, itemId); 
   }
--- src/decorator/Auth.ts
+++ src/decorator/Auth.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { MessageError } from "../model/MessageError";
+import { X_SERVICE_TOKEN } from "../config/Secrets";
 
 export function Auth(
   target: any,
@@ -14,11 +15,13 @@ export function Auth(
     next: NextFunction
   ) {
     const token = req.headers["x-service-token"];
-    console.log(`Headers: ${token}`);
 
     if (token === undefined || token.length === 0) {
         res.status(401).json(new MessageError(401, "Unauthorized, token is mandatory"));
         return;
+    } else if (token !== X_SERVICE_TOKEN) {
+      res.status(401).json(new MessageError(401, "Token not valid"));
+      return;
     }
 
     return originalMethod.apply(this, [req, res, next]);
