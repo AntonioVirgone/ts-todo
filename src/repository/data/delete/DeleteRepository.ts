4cf320846adb3eaabe844805899f0ee4297ca33a
antonio <antonio.virgone@telepass.com>
2024-07-25 12:34:36 +0200

    code review

--- src/repository/data/delete/DeleteRepository.ts
+++ src/repository/data/delete/DeleteRepository.ts
@@ -9,7 +9,7 @@ export class DeleteRepository implements IDeleteRepository {
 
   async delete(userCode: string): Promise<void> {
     try {
-      await write(`${path.join(__dirname, "../../../resources")}/${userCode}.json`, []);
+      await write(`${path.join(__dirname, "../../../../resources")}/${userCode}.json`, []);
     } catch (error) {
       console.error(error);
       throw new Error("Error deleted all items");
@@ -22,7 +22,7 @@ export class DeleteRepository implements IDeleteRepository {
       const newItems: TodoElementModel[] = items.filter(
         (item) => item._id !== itemId
       );
-      await write(`${path.join(__dirname, "../../../resources")}/${userCode}.json`, newItems);
+      await write(`${path.join(__dirname, "../../../../resources")}/${userCode}.json`, newItems);
     } catch (error) {
       console.error(error);
       throw new Error(`Error deleted item with id ${itemId}`);
