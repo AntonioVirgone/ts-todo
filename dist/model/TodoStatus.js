83bd4d1249b34d6992d9947db331cfc1a28e401b
antonio <antonio.virgone@telepass.com>
2024-07-27 14:36:39 +0200

    Update gitignore

--- dist/model/TodoStatus.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TodoStatus = void 0;
-var TodoStatus;
-(function (TodoStatus) {
-    TodoStatus["IN_PROGRESS"] = "IN_PROGRESS";
-    TodoStatus["PENDING"] = "PENDING";
-    TodoStatus["COMPLETED"] = "COMPLETED";
-})(TodoStatus || (exports.TodoStatus = TodoStatus = {}));
